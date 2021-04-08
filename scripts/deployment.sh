6dd8bfab254a6564c550da10e07e9bb6cc083f3e
root <root@LAPTOP-5JKG7OV6.localdomain>
2021-04-08 14:25:26 +0530

    newone

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo cd /home/deployment/focloid-test
+cd /home/deployment/focloid-test
 
 sudo git pull
 
-sudo cd borrower-service && sudo mvn clean install
+cd borrower-service && sudo mvn clean install
 
 sudo service payroll stop
 
-sudo cp -pr target/borrower-service-0.0.1-SNAPSHOT.jar /opt/installer/payroll/
+sudo cp -pr /home/deployment/focloid-test/borrower-service/target/borrower-service-0.0.1-SNAPSHOT.jar /opt/installer/payroll/app.jar
 
 sudo service payroll start
